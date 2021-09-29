ba49816dba34d6bd51c802d7b9e97574ad734000
peniede <pn@zeromar.de>
2021-09-29 18:01:47 +0200

    changed message-form from visibility to opacity

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const targetDiv = document.getElementById("message-form");
+/*const targetDiv = document.getElementById("message-form");
 const btn = document.getElementById("button");
 btn.onclick = function () {
-  if (targetDiv.style.visibility !== "hidden") {
-    targetDiv.style.visibility = "hidden";
+  if (targetDiv.style.opacity !== "0") {
+    targetDiv.style.opacity = "0";
   } else {
-    targetDiv.style.visibility = "visible";
+    targetDiv.style.opacity = "1";
+  }
+};
+*/
+
+const targetDiv = document.getElementById("message-form");
+const btn = document.getElementById("button");
+btn.onclick = function() {
+  if (targetDiv.className === "") {
+    targetDiv.className = "akt";
+  } else {
+    targetDiv.className = "";
   }
 };
